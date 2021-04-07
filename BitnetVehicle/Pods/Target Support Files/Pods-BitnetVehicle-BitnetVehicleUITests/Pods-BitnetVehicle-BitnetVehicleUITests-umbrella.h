2bcf5e877405478af13492fc8e52c052af463365
József Jagicza <>
2021-04-08 00:34:27 +0200

    add cocoapods

---
+++ BitnetVehicle/Pods/Target Support Files/Pods-BitnetVehicle-BitnetVehicleUITests/Pods-BitnetVehicle-BitnetVehicleUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BitnetVehicle_BitnetVehicleUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BitnetVehicle_BitnetVehicleUITestsVersionString[];
+
---
+++ BitnetVehicle/Pods/Target Support Files/Pods-BitnetVehicle/Pods-BitnetVehicle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitnetVehicle : NSObject
+@end
+@implementation PodsDummy_Pods_BitnetVehicle
+@end
