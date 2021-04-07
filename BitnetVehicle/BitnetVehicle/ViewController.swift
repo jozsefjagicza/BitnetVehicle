6a030788863f3142512643dedbff7ad1eb48aceb
József Jagicza <>
2021-04-08 00:12:07 +0200

    first commit

---
+++ BitnetVehicle/BitnetVehicle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitnetVehicle
+//
+//  Created by József Jagicza on 2021. 04. 08..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
